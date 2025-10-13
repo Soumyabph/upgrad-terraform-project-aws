a7f93a1edf344021d44690999561ae4a6166d347
Soumyabph <42370175+Soumyabph@users.noreply.github.com>
2025-10-13 19:12:14 +0530

    Update keypair.tf

--- terraform/modules/vpc/keypair.tf
+++ terraform/modules/vpc/keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "bastionconnect" {
   key_name   = "keypair1"
-public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBN8YvnUcES5HF83w35E297gKl1UFYmIemGwuCIRrNs ubuntu@ip-172-31-29-67"
+public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfi8vUckVNAwGRCNgNcIvMWwCQge9am+GmFa8Rz2cRv ubuntu@ip-172-31-20-209"
 }
